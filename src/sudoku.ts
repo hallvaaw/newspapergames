b15a52c503e3c38b17f65f553e994a8973d1602a
hallvaaw <hallvard_wahler@hotmail.com>
2023-05-01 22:29:31 +0200

    update table

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -33,7 +33,7 @@ function selectColumns(num) {
 
 let sudokuFrame = document.getElementById("sudoku-frame");
 for (let i = 0; i < 9; i++) {
-    let row = document.createElement("div");
+    let row = document.createElement("tr");
     row.id = `row-${i}`;
     row.className = "row";
     sudokuFrame.appendChild(row);
@@ -41,13 +41,18 @@ for (let i = 0; i < 9; i++) {
         row.classList.add("row3");
     }
     for (let j = 0; j < 9; j++) {
-        let cell = document.createElement("input");
+        let cell = document.createElement("td");
+        let cellInput = document.createElement("input");
         cell.id = `cell-${j}`;
+        cellInput.type = "number";
+        cellInput.value = j;
+        cellInput.max = 9;
+        cellInput.id = `cell-input-${j}`;
         cell.className = "cell";
-        cell.innerText = j;
         if (j >= 3 && j % 3 == 0) {
             cell.classList.add("cell3");
         }
+        cell.appendChild(cellInput);
         row.appendChild(cell);
     }
 }
